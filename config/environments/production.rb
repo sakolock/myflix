c316183cbee71aabfd599cbbe21c6e0d3e69c1c6
Steve Kolock <sakolock@gmail.com>
2017-10-29 11:44:20 -0400

    Switches mailer to sendinblue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,13 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'kolock-myflix.heroku.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp-relay.sendinblue.com',
+      :port => 587,
+      :domain => 'kolock-myflix.herokuapp.com',
+      :user_name => ENV['SENDINBLUE_USERNAME'],
+      :password => ENV['SENDINBLUE_PASSWORD'],
+      :authentication => 'login',
+      :enable_starttls_auto => true
+    }
 end
