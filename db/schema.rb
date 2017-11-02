07e0e62770345f6f578d83cd85471fde4721a712
Steve Kolock <sakolock@gmail.com>
2017-11-01 20:35:36 -0400

    Added admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024043421) do
+ActiveRecord::Schema.define(version: 20171030023917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20171024043421) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "token"
+    t.boolean "admin"
   end
 
   create_table "video_categories", force: :cascade do |t|
