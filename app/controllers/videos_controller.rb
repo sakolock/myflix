b14d3482511ccae71605d383756e95c7fdbd6321
Steve Kolock <sakolock@gmail.com>
2017-11-20 18:18:28 -0500

    Adds elastic search features

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,6 +14,20 @@ class VideosController < ApplicationController
     @results = Video.search_by_title(params[:search_term])
   end
 
+  def advanced_search
+    options = {
+      reviews: params[:reviews],
+      rating_from: params[:rating_from],
+      rating_to: params[:rating_to]
+    }
+
+    if params[:query]
+      @videos = Video.search(params[:query], options).records.to_a
+    else
+      @videos = []
+    end
+  end
+
   private
 
   def set_video
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name ["myflix", Rails.env].join('_')
+
   belongs_to :category
   has_many :reviews,-> { order 'created_at DESC' }
 
@@ -10,5 +15,52 @@ class Video < ActiveRecord::Base
   def self.search_by_title(search_term)
     return [] if search_term.blank?
     Video.where("title LIKE ?", "%#{search_term}%").order("created_at DESC")
+  end
+
+  def average_rating
+    reviews.average(:rating).to_f.round(1) if reviews.any?
+  end
+
+  def self.search(query, options={})
+    search_definition = {
+      query: {
+        bool: {
+          must: {
+            multi_match: {
+              query: query,
+              fields: ["title^100", "description^50"],
+              operator: "and"
+            }
+          }
+        }
+      }
+    }
+
+    if query.present? && options[:reviews].present?
+      search_definition[:query][:bool][:must][:multi_match][:fields] << "reviews.content"
+    end
+
+    if options[:rating_from].present? || options[:rating_to].present?
+      search_definition[:query][:bool][:filter] = {
+        range: {
+          average_rating: {
+            gte: (options[:rating_from] if options[:rating_from].present?),
+            lte: (options[:rating_to] if options[:rating_to].present?)
+          }
+        }
+      }
+    end
+
+    __elasticsearch__.search(search_definition)
+  end
+
+  def as_indexed_json(options={})
+    as_json(
+      methods: [:average_rating],
+      only: [:title, :description],
+      include: {
+        reviews: { only: [:content] }
+      }
+    )
   end
 end
